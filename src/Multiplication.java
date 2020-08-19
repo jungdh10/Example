6eb3aae3b9a461166c07a93a23d1cd24299a9d14
dahye <dahye@dahye-MacBook-Pro.local>
2020-08-19 23:01:33 +0900

    upload Multiplication

---
+++ src/Multiplication.java
@@ -0,0 +1,30 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+
+		int i, j =1;
+		int x = 0;
+		
+		
+		for(i =1; i<=9; i++) {
+			for(j=1; j<=9; j++) {
+				x=i * j;
+				System.out.printf("%d*%d=%d\n", i, j, x);
+			
+			
+			
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
