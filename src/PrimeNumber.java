93c9b3280c95c7e8afcfb0e0cfbaec08f65ad19b
jungdh10 <dhh0089@gmail.com>
2021-02-17 22:24:15 +0900

    소수판별 방법1

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -4,6 +4,7 @@ public class PrimeNumber {
 
 	public static void main(String[] args) {
 		//소수 판별하기
+		//소수를 판별할 숫자 i를 2부터 차례로 나누어서 처음으로 나누어 떨어졌을 때 i와 제수가 같으면 소수이다.
 		int i;
 		int j;
 		
@@ -13,7 +14,7 @@ public class PrimeNumber {
 		
 		
 		j=2;
-		while(i%j != 0)
+		while(i%j != 0) 
 			j++;
 		
 			if(i==j) {
@@ -21,6 +22,7 @@ public class PrimeNumber {
 			}else {
 				System.out.println("소수가 아닙니다.");
 			}
+		
 
 	}
 
