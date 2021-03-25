44236e47da7f9f32fd3b9194d6020b644e2fb66d
jungdh10 <dhh0089@gmail.com>
2021-03-25 22:22:23 +0900

    소수 합계(수정)

--- src/PrimeNumber_Sum.java
+++ src/PrimeNumber_Sum.java
@@ -4,33 +4,34 @@ public class PrimeNumber_Sum {
 
 	public static void main(String[] args) {
 		//소수의 합 구하기
+		int a;
 		int i;
 		int j;
-		int x=2;
-		int y;
-		
+		int x=0;
+	
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자 입력:");
-		i = sc.nextInt();
+		a = sc.nextInt();
 		
-		for(j=2; j<=i; j++) {
-			
-			for(y=2; y<=j; y++){
+		for(i=2; i<=a; i++) {			
+			//소수 판별하기
+			for(j=2; j<i; j++) {
+								
+				//소수가 아닌 경우
+				if(i%j == 0) {	
+					break;
+				}					
 				
-				if(j%y != 0) {
-					x+=j;
-					break;
-				}else {
-					break;
-				}
-		
 			}
 			
-			System.out.println(x);
-						
-		}
+			//소수로 판별이 난 경우
+			if(i==j) {
+			System.out.println(j);
+			x+=j;
+			}
+					
+		}		
 		System.out.println("입력한 숫자의 범위에 속하는 모든 소수의 합은" +x+ "입니다.");
-
 	}
 
 }
