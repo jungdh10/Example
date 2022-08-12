c5029c883c297310d5291fcb49b7fabeb4d2ed0b
jungdh10 <dhh0089@gmail.com>
2022-08-12 13:49:20 +0900

    뒤집은 직각삼각형

---
+++ src/multidimensional_array/RightTriangle2.java
@@ -0,0 +1,29 @@
+package multidimensional_array;
+
+public class RightTriangle2 {
+
+	public static void main(String[] args) {
+		//직각 삼각형 모양으로 데이터 채우기
+		int i, j;
+		int k = 0;
+		int a[][] = new int[5][5];
+		
+		for(i = 0; i <= 4; i++) {
+			for(j = 4-i; j <= 4; j++) {
+				k++;
+				a[i][j] = k;
+			}
+		}
+		
+		
+		
+		for(i = 0; i <= 4; i++) {
+			for(j = 0; j <= 4; j++) {
+				System.out.printf("%3d", a[i][j]);
+			}
+			System.out.printf("\n");
+		}
+
+	}
+
+}
