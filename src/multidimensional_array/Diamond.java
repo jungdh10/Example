a38f585f4fb4672ecddc47783e8e2875d146cebf
jungdh10 <dhh0089@gmail.com>
2022-08-24 20:47:23 +0900

    마름모 모양으로 데이터 채우기

---
+++ src/multidimensional_array/Diamond.java
@@ -0,0 +1,40 @@
+package multidimensional_array;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		//다이아몬드 모양으로 데이터 채우기
+		int i, j;
+		int s = 2, e = 2;
+		int k = 0;
+		int a[][] = new int[5][5];
+				
+		for(i = 0; i <= 4; i++) {
+			
+			for(j = s; j <= e; j++) {
+				k++;
+				a[i][j] = k;
+			}
+			
+			if(i >= 2) {
+				s++;
+				e--;
+			}else {
+				s--;
+				e++;
+			}
+			
+		}
+		
+		
+		for(i = 0; i <= 4; i++) {
+			for(j = 0; j <= 4; j++) {
+				System.out.printf("%3d", a[i][j]);
+			}
+			System.out.printf("\n");
+		}
+		
+
+	}
+
+}
