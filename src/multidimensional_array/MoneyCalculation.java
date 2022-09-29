e8a37e1763069fdc9e244a334e7ea016bc78deda
jungdh10 <dhh0089@gmail.com>
2022-09-29 13:52:08 +0900

    화폐 매수 계산하기

---
+++ src/multidimensional_array/MoneyCalculation.java
@@ -0,0 +1,21 @@
+package multidimensional_array;
+
+import java.util.Scanner;
+
+public class MoneyCalculation {
+
+	public static void main(String[] args) {
+		//화폐 종류별 매수 계산하기
+		int unit = 50000; 
+		int num = 0;
+		int sw = 0;
+		int m;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("금액 입력:");
+		m = sc.nextInt();
+
+		
+	}
+
+}
