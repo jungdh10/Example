401f952c1b4b0feb08d1a3182f50de84d1b7fc49
jungdh10 <dhh0089@gmail.com>
2022-10-20 16:25:56 +0900

    역순 구하기(수정)

--- src/multidimensional_array/ReversePlus.java
+++ src/multidimensional_array/ReversePlus.java
@@ -4,17 +4,19 @@ public class ReversePlus {
 
 	public static void main(String[] args) {
 		//역순으로 숫자 더하기
-		int[] a = {1, 2, 3, 4, 5};
-		int[] b = new int[5];
-		int mok, nmg;
-		
-		for(int i=4; i>=0; i--) {
-			mok = a[i] / 10;
-			nmg = a[i] % 10;
-			a[i] = nmg;
+		int[] a = new int[5];
+		int x = 12345;
+		int i, mok, nmg;
+				
+		for(i=0; i<=4; i++) {
+			mok = x / 10; 
+			nmg = x % 10; 
+			a[i] = nmg;	
+			//역순
+			x = mok;
 			System.out.print(a[i]);
-			
 		}
+		
 		
 		
 	}
