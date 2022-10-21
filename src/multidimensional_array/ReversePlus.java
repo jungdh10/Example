d8162452590775b02d40c049d0413c71e361d9b4
jungdh10 <dhh0089@gmail.com>
2022-10-21 23:51:14 +0900

    역순 더하기 추가

--- src/multidimensional_array/ReversePlus.java
+++ src/multidimensional_array/ReversePlus.java
@@ -6,16 +6,33 @@ public class ReversePlus {
 		//역순으로 숫자 더하기
 		int[] a = new int[5];
 		int x = 12345;
-		int i, mok, nmg;
-				
+		int i, mok, nmg, hap;
+		int m = 0;
+		int k = x;
+		
+		
 		for(i=0; i<=4; i++) {
 			mok = x / 10; 
 			nmg = x % 10; 
 			a[i] = nmg;	
 			//역순
 			x = mok;
-			System.out.print(a[i]);
+			
 		}
+		
+		
+		for(int j=0; j<=4; j++) {
+			m += a[j];
+			//1의 자리에는 10을 곱하지 않도록 하기
+			if(j <= 3) {
+				m *= 10;
+			}
+
+		}
+		
+		
+		hap = k + m;
+		System.out.printf("%d + %d = %d\n", k, m, hap);
 		
 		
 		
